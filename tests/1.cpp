926a291e88be2ac590216ad4cc7daebd818cefcb
haoyang9804 <haoyanghkust@gmail.com>
2023-07-01 22:09:15 +0800

    update

---
+++ tests/1.cpp
@@ -0,0 +1,12 @@
+template <class T, class U>
+struct is_same { static constexpr bool value = false; };
+
+template <class T>
+struct is_same<T, T> { static constexpr bool value = true; };
+
+template <class T, class U>
+concept same_as = is_same<T, U>::value;
+
+int const& f();
+
+same_as<int const&> auto& i = f();
